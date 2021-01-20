2f7d0fb56198b88d926914a5529f58588e534bea
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-20 09:39:26 -0400

    Added GameCharacter class and added some code

---
+++ Inheritance/GameCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class GameCharacter
+    {
+        // fields
+        public string name;
+        public int x;
+        public int y;
+        public int health;
+
+        // Methods
+        public void ShowStats()
+        {
+            Console.WriteLine("\n|Name: " + name);
+            Console.WriteLine("|Health: " + health);
+            Console.WriteLine("|X: " + x + " |Y: " + y + "\n");
+        }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,6 +10,26 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("| Inheritance - Bradley Morrison|");
+
+            Player player = new Player();
+            Enemy enemy = new Enemy();
+
+            // Player
+            player.x = 10;
+            player.y = 20;
+            player.health = 95;
+            player.name = "Hero Smith";
+            player.ShowStats();
+
+            // Enemy
+            enemy.x = 50;
+            enemy.y = 80;
+            enemy.health = 100;
+            enemy.name = "Villain Smith";
+            enemy.ShowStats();
+
+            Console.ReadKey(true);
         }
     }
 }
